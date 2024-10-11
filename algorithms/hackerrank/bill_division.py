0845f7cfd6aa183059048e5c0d1049ee9cb1488c
FerSoriano <fer8f10@hotmail.com>
2024-10-10 20:58:12 -0600

    add hackerrank directory

---
+++ algorithms/hackerrank/bill_division.py
@@ -0,0 +1,26 @@
+
+
+def bonAppetit(bill: list, k: list, b: int):
+    """ 
+    bill es la lista de la cuenta
+    k es la lista de lo que Anna NO comio
+    b es la lista de la cuenta separada
+    """
+    total = 0
+    for e in bill:
+        if k != e:
+            total += e
+    
+    anna = total / 2
+    
+    if anna == b:
+        return "Bon Appetit"    
+    
+    return int(b - anna)
+
+
+bill = [3,10,2,9]
+k = bill[1]
+b = 7
+
+print(bonAppetit(bill,k,b))
---
+++ algorithms/hackerrank/flatland.py
@@ -0,0 +1,21 @@
+
+
+def flatlandSapaceStations(n: int, c: list):
+
+    distancias = []
+
+    for e in range(0,n):
+        dist_temp = []
+        for j in c:
+            dist = j - e
+            dist_temp.append(abs(dist))            
+
+        distancias.append(min(dist_temp))
+
+    return max(distancias)
+
+
+n = 12
+c = [3,8]
+
+print(flatlandSapaceStations(n,c))
