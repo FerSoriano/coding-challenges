b867efc8082e00c77e81a496953612ec28b05ab7
FerSoriano <fer8f10@hotmail.com>
2025-08-18 18:08:03 -0600

    change the folder structure. Add hackerrank folder

--- hackerrank/bill_division.py
+++ hackerrank/bill_division.py
@@ -1,7 +1,7 @@
 
 
 def bonAppetit(bill: list, k: list, b: int):
-    """ 
+    """
     bill es la lista de la cuenta
     k es la lista de lo que Anna NO comio
     b es la lista de la cuenta separada
@@ -10,17 +10,17 @@ def bonAppetit(bill: list, k: list, b: int):
     for e in bill:
         if k != e:
             total += e
-    
+
     anna = total / 2
-    
+
     if anna == b:
-        return "Bon Appetit"    
-    
+        return "Bon Appetit"
+
     return int(b - anna)
 
 
-bill = [3,10,2,9]
+bill = [3, 10, 2, 9]
 k = bill[1]
 b = 7
 
-print(bonAppetit(bill,k,b))
+print(bonAppetit(bill, k, b))
